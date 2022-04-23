0884400635c66d23de93000bcaa42c8d2cfe6356
RisyadFaisalH <innovation.faisal@gmail.com>
2022-04-23 14:06:14 +0700

    refactor

--- frontend/pages/login/register.jsx
+++ frontend/pages/login/register.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import CanvasLanding from "../../components/canvasLanding";
+import Link from "next/link";
 
 function Register() {
   return (
@@ -8,8 +9,15 @@ function Register() {
         <h1 className="font-black">On Time</h1>
         <h2>Register</h2>
       </div>
-      <form className="text-white mx-5 mt-14 flex flex-col gap-1">
-        <label className="font-light text-sm">Username</label>
+      <form className="text-white mx-5 mt-5 flex flex-col gap-1">
+      <label className="font-light text-sm">Name</label>
+        <input
+          className="border-2 border-transparent bg-stone-800 opacity-75 h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none placeholder-white w-full"
+          type="text"
+          name="search"
+          placeholder="Name"
+        />
+        <label className="font-light mt-5 text-sm">Username</label>
         <input
           className="border-2 border-transparent bg-stone-800 opacity-75 h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none placeholder-white w-full"
           type="text"
@@ -34,11 +42,15 @@ function Register() {
           Register
         </button>
       </form>
-      <div className="text-center text-xs text-white font-light mx-5 mt-20">
-        <h3>Already Have an Account?</h3>
-        <button className="h-8 w-full mt-2 text-xs bg-stone-800 opacity-75 rounded-md text-white hover:opacity-80 transition duration-300 flex justify-center items-center">
-          Login
-        </button>
+      <div className="text-center text-xs text-white font-light mx-5 mt-14">
+        <h3>
+          Already Have an Account?{" "}
+          <Link href={"/login"}> 
+            <a className="font-bold hover:opacity-80 transition">
+            Here
+            </a>
+          </Link>
+        </h3>
       </div>
     </CanvasLanding>
   );
