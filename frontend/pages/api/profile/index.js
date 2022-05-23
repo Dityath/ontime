a600d20df27b88e7e15002af0c317f18c49f41ab
farhanakbarr008 <farhanakbarr008@gmail.com>
2022-05-23 23:59:43 +0700

    give documentations to the backend codes

--- frontend/pages/api/profile/index.js
+++ frontend/pages/api/profile/index.js
@@ -1,12 +1,19 @@
+// Import needed libraries and dependencies
+
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient(); // Define prisma client
 
+// Function to handle profile page (get user data to display in profile page)
 export default async function handler(req, res) {
   switch (req.method) {
+    // Profile will only have GET method
     case "GET": {
+      // Get userId data from request body
+      const userId = req.body.userId;
+      // Try to find user data with the same id from userId using
+      // prisma.findFirst() (https://www.prisma.io/docs/reference/api-reference/prisma-client-reference#findfirst)
       try {
-        const userId = req.body.userId;
         const user = await prisma.user.findFirst({
           where: { id: userId },
         });
@@ -16,6 +23,7 @@ export default async function handler(req, res) {
       }
     }
     default:
+      // If the method is not "GET", send exception "Method Not Allowed"
       res.status(404).send({ message: "Method Not Allowed" });
       break;
   }
